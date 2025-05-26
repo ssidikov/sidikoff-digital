5bc6e8dbf32fb5bd2721858daf4f6a9e2954e311
Sardorbek SIDIKOV <sardorbek.sidikov7@gmail.com>
2025-05-26 15:57:22 +0200

    feat: add Google Tag Manager integration with script and noscript tags for improved tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -68,10 +68,35 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   `}
         </Script>
 
+        {/* GTM script */}
+        {process.env.NEXT_PUBLIC_GTM_ID && (
+          <Script id='gtm-script' strategy='afterInteractive'>
+            {`
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id=${process.env.NEXT_PUBLIC_GTM_ID}'+dl;
+      f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
+    `}
+          </Script>
+        )}
+
         {/* Structured Data */}
         <StructuredData type='all' />
       </head>
       <body className={inter.className}>
+        {/* GTM noscript */}
+        {process.env.NEXT_PUBLIC_GTM_ID && (
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+              height='0'
+              width='0'
+              style={{ display: 'none', visibility: 'hidden' }}></iframe>
+          </noscript>
+        )}
+
         <ThemeProvider
           attribute='class'
           defaultTheme='system'
